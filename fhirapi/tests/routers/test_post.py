21e27312092a1d150be2c29e57a0aab9eea32608
Matt Koch <matthewjameskoch@gmail.com>
2025-11-14 12:32:44 -0500

    add router tests

---
+++ fhirapi/tests/routers/test_post.py
@@ -0,0 +1,26 @@
+import pytest
+from httpx import AsyncClient
+
+
+async def create_post(body: str, async_client: AsyncClient) -> dict:
+    response = await async_client.post("/post", json={"body": body})
+    return response.json()
+
+
+# Fixture to create a post, no autouse=True because it's used in other tests
+@pytest.fixture()
+async def created_post(async_client: AsyncClient):
+    return await create_post("Test post", async_client)
+
+
+@pytest.mark.anyio
+async def test_create_post(async_client: AsyncClient):
+    body = "Test post"
+
+    response = await async_client.post(
+        "/post",
+        json={"body": body},
+    )
+
+    assert response.status_code == 201
+    assert {"id": 0, "body": body}.items() <= response.json().items()
