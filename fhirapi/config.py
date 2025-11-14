eff52a47d0eab98c34a9a372850fa7a01162efb0
Matt Koch <matthewjameskoch@gmail.com>
2025-11-14 14:54:05 -0500

    feat: :wrench: adds config
    Add a config.py file that reads from a .env file.

---
+++ fhirapi/config.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class BaseConfig(BaseSettings):
+    class Config:
+        env_file: str = ".env"
+
+
+class GlobalConfig(BaseConfig):
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
