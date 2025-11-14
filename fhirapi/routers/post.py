da1c3357918225438d9eba4879972f2f20d78c16
Matt Koch <matthewjameskoch@gmail.com>
2025-11-14 09:26:46 -0500

    swap dict for model_dump

--- fhirapi/routers/post.py
+++ fhirapi/routers/post.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter
 
-from fhirapi.models.post import UserPost, UserPostIn
+from fhirapi.models.post import Comment, CommentIn, UserPost, UserPostIn
 
 router = APIRouter()
 post_table = {}
+comment_table = {}
+
+
+def find_post(post_id: int):
+    return post_table.get(post_id)
 
 
 @router.post("/post", response_model=UserPost)
 async def create_post(post: UserPostIn):
-    data = post.dict()
+    data = post.model_dump()
     last_record_id = len(post_table)
     new_post = {**data, "id": last_record_id}
     post_table[last_record_id] = new_post
@@ -18,3 +23,12 @@ async def create_post(post: UserPostIn):
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
     return list(post_table.values())
+
+
+@router.post("/comment", response_model=Comment)
+async def create_comment(comment: CommentIn):
+    data = comment.model_dump()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
