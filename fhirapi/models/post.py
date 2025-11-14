f367dc5820dd5fa68fc384db3fcdf0265821461c
Matt Koch <matthewjameskoch@gmail.com>
2025-11-14 09:21:08 -0500

    comment models

--- fhirapi/models/post.py
+++ fhirapi/models/post.py
@@ -7,3 +7,12 @@ class UserPostIn(BaseModel):
 
 class UserPost(UserPostIn):
     id: int
+
+
+class CommentIn(BaseModel):
+    body: str
+    post_id: int
+
+
+class Comment(CommentIn):
+    id: int
