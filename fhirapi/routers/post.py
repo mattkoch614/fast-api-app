460eb4fb8a608f397fcb7ac2f71106b9473e9c93
Matt Koch <matthewjameskoch@gmail.com>
2025-11-14 09:40:16 -0500

    get post with comments

--- fhirapi/routers/post.py
+++ fhirapi/routers/post.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from fhirapi.models.post import Comment, CommentIn, UserPost, UserPostIn
+from fhirapi.models.post import (
+    Comment,
+    CommentIn,
+    UserPost,
+    UserPostIn,
+    UserPostWithComments,
+)
 
 router = APIRouter()
 post_table = {}
@@ -11,7 +17,7 @@ def find_post(post_id: int):
     return post_table.get(post_id)
 
 
-@router.post("/post", response_model=UserPost)
+@router.post("/post", response_model=UserPost, status_code=201)
 async def create_post(post: UserPostIn):
     data = post.model_dump()
     last_record_id = len(post_table)
@@ -25,10 +31,33 @@ async def get_all_posts():
     return list(post_table.values())
 
 
-@router.post("/comment", response_model=Comment)
+@router.post("/comment", response_model=Comment, status_code=201)
 async def create_comment(comment: CommentIn):
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
     data = comment.model_dump()
     last_record_id = len(comment_table)
     new_comment = {**data, "id": last_record_id}
     comment_table[last_record_id] = new_comment
     return new_comment
+
+
+@router.get("/post/{post_id}/comment", response_model=list[Comment])
+async def get_comments_on_post(post_id: int):
+    return [
+        comment for comment in comment_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@router.get("/post/{post_id}", response_model=UserPostWithComments)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    # await is used to wait for the asynchronous function to complete
+    return {
+        "post": post,
+        "comments": await get_comments_on_post(post_id),
+    }
