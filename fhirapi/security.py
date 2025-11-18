090c349fedca5ac4d31d4e3dbc9881b805a1e4fe
Matt Koch <matthewjameskoch@gmail.com>
2025-11-18 18:48:06 -0500

    swap with bcrypt

--- fhirapi/security.py
+++ fhirapi/security.py
@@ -1,20 +1,20 @@
 import logging
 
-from passlib.context import CryptContext
+import bcrypt
 
 from fhirapi.database import database, user_table
 
 logger = logging.getLogger(__name__)
 
-pwd_context = CryptContext(schemes=["bcrypt"])
-
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+    )
 
 
 async def get_user(email: str):
