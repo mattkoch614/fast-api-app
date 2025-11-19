0ca55e3f288c923f1bcf2de20d1359cd77f03d07
Matt Koch <matthewjameskoch@gmail.com>
2025-11-18 19:02:34 -0500

    user pass hashing

--- fhirapi/tests/test_security.py
+++ fhirapi/tests/test_security.py
@@ -3,6 +3,11 @@ import pytest
 from fhirapi import security
 
 
+def test_password_hash():
+    password = "password"
+    assert security.verify_password(password, security.get_password_hash(password))
+
+
 @pytest.mark.anyio
 async def test_get_user(registered_user: dict):
     user = await security.get_user(registered_user["email"])
