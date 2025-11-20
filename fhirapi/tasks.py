f20113b5802b9397fa8e3a553966d1c02dda929b
Matt Koch <matthewjameskoch@gmail.com>
2025-11-20 17:03:03 -0500

    test task for images

--- fhirapi/tasks.py
+++ fhirapi/tasks.py
@@ -1,8 +1,11 @@
 import logging
+from json import JSONDecodeError
 
 import httpx
+from databases import Database
 
 from fhirapi.config import config
+from fhirapi.database import post_table
 
 logger = logging.getLogger(__name__)
 
@@ -45,3 +48,67 @@ async def send_user_registration_email(email: str, confirmation_url: str):
             ),
         ),
     )
+
+
+# Internal function to generate a cute creature API for a prompt
+async def _generate_cute_creature_api(prompt: str):
+    """
+    Internal function to generate a cute creature API for a prompt
+    """
+    logger.debug(f"Generating cute creature API for prompt: {prompt}")
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                "https://api.deepai.org/api/text-to-image",
+                json={"prompt": prompt},
+                headers={"Authorization": f"Bearer {config.DEEPAI_API_KEY}"},
+                timeout=60,
+            )
+            logger.debug(response)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as err:
+            raise APIResponseError(
+                f"API request failed with status code: {err.response.status_code}"
+            ) from err
+        except (JSONDecodeError, TypeError) as err:
+            raise APIResponseError(
+                f"API request failed with invalid response: {err}"
+            ) from err
+
+
+async def generate_and_add_to_post(
+    email: str,
+    post_id: int,
+    post_url: str,
+    database: Database,
+    prompt: str = "A cute newfoundland puppy with a pink bow on its head",
+):
+    try:
+        response = await _generate_cute_creature_api(prompt)
+    except APIResponseError:
+        return await send_simple_email(
+            email,
+            "Error generating image",
+            f"Hi {email}! Unfortunately, there was an error generating image"
+            " for your post.",
+        )
+    logger.debug("Connecting to database to update post with image URL")
+
+    query = (
+        post_table.update()
+        .where(post_table.c.id == post_id)
+        .values(image_url=response["output_url"])
+    )
+
+    logger.debug(query)
+    await database.execute(query)
+    logger.debug("Database connection in background task closed")
+
+    await send_simple_email(
+        email,
+        "Image generated successfully",
+        f"Hi {email}! Your image has been generated successfully and added to your post. "
+        " Please click the link to view your post: {post_url}",
+    )
+    return response
--- fhirapi/tests/test_tasks.py
+++ fhirapi/tests/test_tasks.py
@@ -1,7 +1,11 @@
 import httpx
 import pytest
 
-from fhirapi.tasks import APIResponseError, send_simple_email
+from fhirapi.tasks import (
+    APIResponseError,
+    _generate_cute_creature_api,
+    send_simple_email,
+)
 
 
 @pytest.mark.anyio
@@ -18,3 +22,16 @@ async def test_send_simple_email_error(mock_httpx_client):
 
     with pytest.raises(APIResponseError):
         await send_simple_email("test@example.com", "Test Subject", "Test Body")
+
+
+@pytest.mark.anyio
+async def test_generate_cute_creature_api_success(mock_httpx_client):
+    json_data = {"output_url": "https://fakeurl.cm/image.jpg"}
+    mock_httpx_client.post.return_value = httpx.Response(
+        status_code=200, json=json_data, request=httpx.Request("POST", "//")
+    )
+
+    result = await _generate_cute_creature_api(
+        "A cute newfoundland puppy with a pink bow on its head"
+    )
+    assert result == json_data
